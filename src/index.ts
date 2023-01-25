1495ec6df8f3eecaec9ee410acdc8d4d0696b05b
laserzz <82984448+laserzz@users.noreply.github.com>
2023-01-24 18:54:42 -0800

    maybe fix?

--- src/index.ts
+++ src/index.ts
@@ -3,7 +3,7 @@ import dotenv from 'dotenv';
 
 dotenv.config()
 
-export const bot = new BotClient({auth: process.env.TOKEN, collectionLimits: {messages: 3}, gateway: {maxShards: 'auto'}});
+export const bot = new BotClient({auth: process.env.TOKEN, collectionLimits: {messages: 10, members: Infinity, users: Infinity}, gateway: {maxShards: 'auto'}});
 bot.startEventHandler();
 
 process.on('uncaughtException', (err) => {
