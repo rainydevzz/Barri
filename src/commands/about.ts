5b62d494d76a32a6f66fcb4cf52b2b75accc5754
laserzz <82984448+laserzz@users.noreply.github.com>
2023-01-24 19:47:31 -0800

    invs to about

--- src/commands/about.ts
+++ src/commands/about.ts
@@ -6,7 +6,17 @@ export async function execute(interaction: ExtInteraction): Promise<void> {
         description: 'Hello! I am a moderation bot with several useful utilities. Run /help for more info!',
         color: 0x00008b,
         timestamp: new Date().toISOString(),
-        thumbnail: {url: interaction.client.user.avatarURL("png")}
+        thumbnail: {url: interaction.client.user.avatarURL("png")},
+        fields: [
+            {
+                name: 'Invite',
+                value: "[here](https://discord.com/api/oauth2/authorize?client_id=1020163915189067816&permissions=1376537078806&scope=bot%20applications.commands)"
+            },
+            {
+                name: 'Support',
+                value: "[here](https://discord.gg/q3z4EyWBNe)"
+            }
+        ]
     }
 
     await interaction.createMessage({embeds: [embed]});
