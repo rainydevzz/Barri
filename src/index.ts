e688a109f8b49317e6a4ef81ffbf4410039c2f3b
laserzz <82984448+laserzz@users.noreply.github.com>
2023-01-31 00:13:11 -0800

    presence!

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,25 @@
 import { BotClient } from './bot';
 import dotenv from 'dotenv';
+import { ActivityTypes } from 'oceanic.js';
 
 dotenv.config()
 
-export const bot = new BotClient({auth: process.env.TOKEN, collectionLimits: {messages: 10, members: Infinity, users: Infinity}, gateway: {maxShards: 'auto'}});
+export const bot = new BotClient(
+    {
+        auth: process.env.TOKEN,
+        collectionLimits: {messages: 10, members: Infinity, users: Infinity}, 
+        gateway: {
+            maxShards: 'auto',
+            presence: {
+                activities: [{
+                    type: ActivityTypes.WATCHING, name: 'your server'
+                }],
+                status: 'online'
+            }
+        },
+    }
+);
+
 bot.startEventHandler();
 
 process.on('uncaughtException', (err) => {
