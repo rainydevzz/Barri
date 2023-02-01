cafeaec145e52a15aa991a81c58ae8c2220f8657
laserzz <82984448+laserzz@users.noreply.github.com>
2023-01-31 16:31:33 -0800

    small fix

--- src/commands/mute.ts
+++ src/commands/mute.ts
@@ -2,7 +2,7 @@ import { ExtInteraction } from "../types/extinteraction";
 
 export async function execute(interaction: ExtInteraction) {
     let guild = interaction.client.guilds.find(g => g.id == interaction.guildID);
-    let mem = guild.members.find(m => m.id == interaction.options.get('user'));
+    let mem = guild.members.find(m => m.id == interaction.options.get('member'));
     let min = interaction.options.get('duration');
     let duration = (min * 60000) + new Date().getTime();
     let durtime = new Date(duration);
