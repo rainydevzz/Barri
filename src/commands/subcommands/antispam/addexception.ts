416e85fb73402e8e3d70c6009f3247c7efdb7258
laserzz <82984448+laserzz@users.noreply.github.com>
2023-02-07 17:24:35 -0800

    small fix

--- src/commands/subcommands/antispam/addexception.ts
+++ src/commands/subcommands/antispam/addexception.ts
@@ -3,9 +3,10 @@ import { ExtInteraction } from "../../../types/extinteraction";
 export async function execute(interaction: ExtInteraction) {
     await interaction.defer();
     const res = await interaction.client.db.ignoretable.findFirst({
-        where: {
-            id: interaction.options.get('id')
-        }
+        where: {AND: {
+            id: interaction.options.get('id'),
+            guild: interaction.guildID
+        }}
     });
 
     if(!res) {
